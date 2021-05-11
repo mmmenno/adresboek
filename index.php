4a0edad49debcbba788471b4f4a5480f128abf5b
Menno den Engelse <menno@denengelse.nl>
2021-05-11 22:26:00 +0200

    stamen map

--- index.php
+++ index.php
@@ -129,9 +129,12 @@ include("streets.php");
     }).addTo(map);
     */
 
-    overviewLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    overviewLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      subdomains: 'abcd',
+      minZoom: 0,
       maxZoom: 15,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      ext: 'png'
     }).addTo(map);
     
 
