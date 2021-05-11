9b8b828823a74c4517808131277a7a1f9bbbb456
Menno den Engelse <menno@denengelse.nl>
2021-05-11 22:20:08 +0200

    trying openstreetmap only

--- index.php
+++ index.php
@@ -80,7 +80,7 @@ include("streets.php");
   $(document).ready(function() {
 
     createMap();
-    //refreshMap();
+    refreshMap();
 
     document.body.onkeydown = function(e){
       if(e.keyCode == 32){
@@ -122,11 +122,18 @@ include("streets.php");
     }).addTo(map);
     */
 
+    /*
     overviewLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
       maxZoom: 15,
       attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
     }).addTo(map);
+    */
 
+    overviewLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 15,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    
 
     baseLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
